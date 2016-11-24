661b3fb970a43d3535353773f95b83c3c3ba78f0
SamAn09 <saman_antwithwings@hotmail.com>
2016-11-24 14:58:18 +0545

    This is my first Commit

---
+++ seedblog/blogapp/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class BlogPost(models.Model):
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+
+	def __str__(self):
+		return self.title
+
